c53713dd54a50a819ff60f6f9d03517c949bb0c9
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-04 10:04:31 -0700

    Create 610_triangle_judgement.sql

---
+++ 610_triangle_judgement.sql
@@ -0,0 +1,66 @@
+--Example
+Create table If Not Exists Triangle (x int, y int, z int)
+Truncate table Triangle
+insert into Triangle (x, y, z) values ('13', '15', '30')
+insert into Triangle (x, y, z) values ('10', '20', '15')
+
+
+--My solution
+SELECT
+    *,
+    CASE
+        WHEN (x + y > z) AND (z + x > y) AND (y + z > x) THEN 'Yes' ELSE 'No'
+    END AS triangle
+FROM Triangle
+
+
+
+--Other solution
+select 
+    *,
+    IF(x+y>z AND y+z>x AND z+x>y, "Yes", "No") AS triangle 
+FROM Triangle
+
+
+/*
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+
+
+*/
