6bb34b00fd3c56b0f9a46243d5a97cf5b4915806
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-05 09:50:49 -0700

    Update 619_biggest_single_number.sql
    Add example

--- 619_biggest_single_number.sql
+++ 619_biggest_single_number.sql
@@ -1,3 +1,18 @@
+--Example
+Create table If Not Exists MyNumbers (num int)
+Truncate table MyNumbers
+insert into MyNumbers (num) values ('8')
+insert into MyNumbers (num) values ('8')
+insert into MyNumbers (num) values ('3')
+insert into MyNumbers (num) values ('3')
+insert into MyNumbers (num) values ('1')
+insert into MyNumbers (num) values ('4')
+insert into MyNumbers (num) values ('5')
+insert into MyNumbers (num) values ('6')
+
+
+
+--Solution
 SELECT MAX(num) AS num
 FROM (
     SELECT 
