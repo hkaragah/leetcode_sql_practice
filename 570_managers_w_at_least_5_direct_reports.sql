20a96217cdea99ae6fb11ed6d11580db5dd9ac91
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-05 09:48:05 -0700

    Update 570_managers_w_at_least_5_direct_reports.sql
    Add example

--- 570_managers_w_at_least_5_direct_reports.sql
+++ 570_managers_w_at_least_5_direct_reports.sql
@@ -1,3 +1,16 @@
+--Example
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int)
+Truncate table Employee
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 'None')
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101')
+
+
+
+--Solution #1
 SELECT
     m.name
 FROM
@@ -13,24 +26,79 @@ HAVING
 
 
 
+--Solution #2
+WITH Manager AS (
+    SELECT
+        m.name,
+        m.id,
+        COUNT(e.id) AS report_count
+    FROM
+        Employee AS m
+    INNER JOIN
+        Employee AS e
+        ON e.managerID = m.id
+    GROUP BY
+        m.name,
+        m.id
+)
+SELECT
+    name
+FROM
+    Manager
+WHERE
+    report_count > 4
 
--- WITH Manager AS (
---     SELECT
---         m.name,
---         m.id,
---         COUNT(e.id) AS report_count
---     FROM
---         Employee AS m
---     INNER JOIN
---         Employee AS e
---         ON e.managerID = m.id
---     GROUP BY
---         m.name,
---         m.id
--- )
--- SELECT
---     name
--- FROM
---     Manager
--- WHERE
---     report_count > 4
+
+
+
+/*
+570. Managers with at Least 5 Direct Reports
+
+
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+
+*/
