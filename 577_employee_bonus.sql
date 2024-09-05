a744af1a6eea7b481314b849c5fd6976c61b6a29
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-05 09:49:27 -0700

    Update 577_employee_bonus.sql
    Add example

--- 577_employee_bonus.sql
+++ 577_employee_bonus.sql
@@ -1,3 +1,17 @@
+--Example
+Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int)
+Create table If Not Exists Bonus (empId int, bonus int)
+Truncate table Employee
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', 'None', '4000')
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000')
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000')
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000')
+Truncate table Bonus
+insert into Bonus (empId, bonus) values ('2', '500')
+insert into Bonus (empId, bonus) values ('4', '2000')
+
+
+--Solution
 SELECT
     e.name,
     b.bonus
@@ -8,3 +22,78 @@ LEFT JOIN Bonus AS b
 WHERE
     bonus < 1000
     OR bonus IS NULL
+
+
+
+
+/*
+577. Employee Bonus
+
+
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+
+Table: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+ 
+
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
+
+
+
+*/
