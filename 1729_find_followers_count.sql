bee183cf2e69ee1f17ad226824a6331f183a519a
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-05 09:39:25 -0700

    Update 1729_find_followers_count.sql
    Add example

--- 1729_find_followers_count.sql
+++ 1729_find_followers_count.sql
@@ -1,3 +1,13 @@
+--Example
+Create table If Not Exists Followers(user_id int, follower_id int)
+Truncate table Followers
+insert into Followers (user_id, follower_id) values ('0', '1')
+insert into Followers (user_id, follower_id) values ('1', '0')
+insert into Followers (user_id, follower_id) values ('2', '0')
+insert into Followers (user_id, follower_id) values ('2', '1')
+
+
+--Solution
 SELECT
     user_id,
     COUNT(follower_id) AS followers_count
