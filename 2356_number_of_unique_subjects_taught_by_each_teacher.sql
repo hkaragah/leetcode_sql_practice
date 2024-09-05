e4f3f09d17fdc9b370565bb52954be4db55982ce
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-05 09:44:30 -0700

    Update 2356_number_of_unique_subjects_taught_by_each_teacher.sql
    Add example

--- 2356_number_of_unique_subjects_taught_by_each_teacher.sql
+++ 2356_number_of_unique_subjects_taught_by_each_teacher.sql
@@ -1,3 +1,16 @@
+--Example
+Create table If Not Exists Teacher (teacher_id int, subject_id int, dept_id int)
+Truncate table Teacher
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '3')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '4')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '3', '3')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '1', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '2', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '3', '1')
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '4', '1')
+
+
+--Solution
 SELECT
     teacher_id,
     COUNT(DISTINCT subject_id) AS cnt
