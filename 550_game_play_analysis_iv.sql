bd92ae5982bce09d65f179a4dc904a5bb482d7ba
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-05 09:46:33 -0700

    Update 550_game_play_analysis_iv.sql
    Add example

--- 550_game_play_analysis_iv.sql
+++ 550_game_play_analysis_iv.sql
@@ -1,3 +1,16 @@
+--Example
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int)
+Truncate table Activity
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5')
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-02', '6')
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0')
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5')
+
+
+
+
+--Solution
 WITH MinEventDates AS (
     SELECT player_id, MIN(event_date) AS min_event_date
     FROM Activity
@@ -23,26 +36,55 @@ JOIN MinEventDates AS m
 
 
 
+/*
+550. Game Play Analysis IV
 
 
 
 
+Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-03-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+
+| fraction  |
++-----------+
+| 0.33      |
++-----------+
+Explanation: 
+Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
+
+*/
 
 
-
--- Correct, but inefficient version (time limit exceeded)
--- SELECT
---     ROUND(
---         COUNT(
---             CASE
---                 WHEN event_date = (
---                     SELECT MIN(event_date) + INTERVAL 1 DAY
---                     FROM Activity AS sub
---                     WHERE sub.player_id = Activity.player_id
---                 ) THEN 1
---                 ELSE NULL
---             END
---         ) /
---         COUNT(DISTINCT player_id), 2
---     ) AS fraction
--- FROM Activity
