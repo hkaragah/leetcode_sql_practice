f6f4803d567321f39e015f6e233396405924f4db
Hossein Karagah <42470308+hkaragah@users.noreply.github.com>
2024-09-06 13:18:40 -0700

    Create 1164_product_price_at_a_given_date.sql

---
+++ 1164_product_price_at_a_given_date.sql
@@ -0,0 +1,101 @@
+--Example
+Create table If Not Exists Products (product_id int, new_price int, change_date date)
+Truncate table Products
+insert into Products (product_id, new_price, change_date) values ('1', '20', '2019-08-14')
+insert into Products (product_id, new_price, change_date) values ('2', '50', '2019-08-14')
+insert into Products (product_id, new_price, change_date) values ('1', '30', '2019-08-15')
+insert into Products (product_id, new_price, change_date) values ('1', '35', '2019-08-16')
+insert into Products (product_id, new_price, change_date) values ('2', '65', '2019-08-17')
+insert into Products (product_id, new_price, change_date) values ('3', '20', '2019-08-18')
+
+
+--Solution #1
+SELECT 
+    p.product_id,
+    COALESCE(
+        (SELECT new_price 
+         FROM Products p2 
+         WHERE p2.product_id = p.product_id 
+           AND p2.change_date <= '2019-08-16'
+         ORDER BY p2.change_date DESC 
+         LIMIT 1),
+        10) AS price
+FROM 
+    (SELECT DISTINCT product_id FROM Products) p
+
+
+
+--Solution #2
+SELECT 
+    DISTINCT product_id, 
+    10 AS price 
+FROM Products 
+WHERE product_id NOT IN (
+    SELECT DISTINCT product_id 
+    FROM Products 
+    WHERE change_date <='2019-08-16'
+    )
+UNION 
+SELECT 
+    product_id, 
+    new_price AS price 
+FROM Products 
+WHERE (product_id,change_date) IN (
+    SELECT 
+        product_id, 
+        MAX(change_date) AS date 
+    FROM Products 
+    WHERE change_date <='2019-08-16' 
+    GROUP BY product_id
+    )
+
+
+
+/*
+1164. Product Price at a Given Date
+
+
+Table: Products
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| product_id    | int     |
+| new_price     | int     |
+| change_date   | date    |
++---------------+---------+
+(product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+Each row of this table indicates that the price of some product was changed to a new price at some date.
+ 
+
+Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++------------+-----------+-------------+
+| product_id | new_price | change_date |
++------------+-----------+-------------+
+| 1          | 20        | 2019-08-14  |
+| 2          | 50        | 2019-08-14  |
+| 1          | 30        | 2019-08-15  |
+| 1          | 35        | 2019-08-16  |
+| 2          | 65        | 2019-08-17  |
+| 3          | 20        | 2019-08-18  |
++------------+-----------+-------------+
+Output: 
++------------+-------+
+| product_id | price |
++------------+-------+
+| 2          | 50    |
+| 1          | 35    |
+| 3          | 10    |
++------------+-------+
+*/
